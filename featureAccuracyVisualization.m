30f6ee0b57d2af6b2e06885415ef38091452ef6a
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-10-21 14:53:49 -0400

    add final metrics

--- featureAccuracyVisualization.m
+++ featureAccuracyVisualization.m
@@ -26,6 +26,7 @@ all_features_labels = {
     'Mean Power'
 };
 
+% create feature accuracy chart
 figure(); hold on;
 fontsize(gcf, 12, "points")
 title('5-Fold Cross Validation Training Accuracy by Feature')
@@ -34,8 +35,36 @@ ylabel('Accuracy');
 bar(all_features_labels, all_feature_accuracies);
 gca.Toolbar.Visible = 'off';
 exportgraphics(gcf, strcat("plots/training-accuracy-cross.png"),'Resolution',300)
+close all;
 
+% create the final model and save accuracy metrics
+best_two_features = {'waveformlength_', 'rootmeansquared_'};
+[train_X, train_Y] = loadFeaturesToXY(strcat("features/train/",dataset), best_two_features);
+[test_X, test_Y] = loadFeaturesToXY(strcat("features/test/",dataset), best_two_features);
 
+% metrics for final cross train
+cross_Y_predict = trainCrossValidate(train_X, train_Y, 5);
+[cross_train_accuracy, cross_train_f1_rock, cross_train_f1_paper, cross_train_f1_scissors] = calculateMetrics(cross_Y_predict, train_Y);
+
+% metrics for final cross test
+final_model = createModel(train_X, train_Y);
+test_Y_predict = final_model.predict(test_X);
+[test_accuracy, test_f1_rock, test_f1_paper, test_f1_scissors] = calculateMetrics(test_Y_predict, test_Y);
+
+% save table
+final_metrics_table = array2table( ...
+    [cross_train_accuracy, test_accuracy; ...
+    cross_train_f1_rock, test_f1_rock;  ...
+    cross_train_f1_paper, test_f1_paper; ...
+    cross_train_f1_scissors, test_f1_scissors]', ...
+    'VariableNames', {'Accuracy', 'F1 Rock', 'F1 Paper', 'F1 Scissors'}, ...
+    'RowNames', {'Cross Validation', 'Test'});
+
+fig = uifigure('Position',[0 0 500 76]);
+uit = uitable(fig, "Data", final_metrics_table);
+uit.Position = [0 0 500 76];
+exportapp(fig, strcat("plots/final_metrics.png"))
+close(findall(0, 'type', 'figure'));
 
 
 function [accuracy] = get_accuracy_for_feature_list(dataset, feature_list)
