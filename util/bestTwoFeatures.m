a5b25f6852c69a75bc445fc7f75b5eb99b9b8105
Daniel Sudzilouski <daniel@sudzilouski.com>
2023-11-11 15:51:38 -0500

    add best two features script

---
+++ util/bestTwoFeatures.m
@@ -0,0 +1,5 @@
+% bestTwoFeatures()
+%   returns the current best two features that we are using
+function [best_features] = bestTwoFeatures()
+    best_features = {'waveformlength', 'rootmeansquared'}; 
+end
